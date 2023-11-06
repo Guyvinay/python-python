ad2744587b5f84db09d4d18cbfe6ded343482062
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-11-06 05:32:23 +0530

    User-Service-flas

--- Flask-03-Advance-CRUD/app.py
+++ Flask-03-Advance-CRUD/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, jsonify, request, redirect, render_template
+from flask_login import current_user
 from pymongo import MongoClient 
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
@@ -44,62 +45,6 @@ def register() :
          'user':str(user)
          })
 
-#login endpoint
-
-@app.route('/login', methods=['POST'])
-def login() :
-    data = request.get_json()
-    email = data['email']
-    password = data['password']
-
-    user = db.users.find_one({'email':email})
-    if user and bcrypt.check_password_hash(user['password'],password) :
-        user_obj = User( user['name'], user['email'],user['password'])
-        print(user_obj)
-        return jsonify(
-            {
-                'message':'Login SuccessFul',
-                'user':str(user)
-            }
-        )
-    return jsonify({
-        'message':'Login Failed!'
-    })
-
-
-
-#With Template 
-# @app.route('/register', methods=['GET','POST'])
-# def register() :
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         password = request.form['password']
-        
-#         encoded_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
-#         user = {
-#             'name':name,
-#             'email':email,
-#             'password':encoded_password
-#         }
-
-#         db.users.insert_one(user)
-
-#         # flash('Account has succesfully been created', 'success')
-
-#         return redirect('/login')
-#     return render_template('register.html')
-
-
-
-
-# app.py
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     # Login logic goes here
-#     return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
