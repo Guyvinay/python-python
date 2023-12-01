ce37ea596031195c585a365dd8a873b0cc074c1f
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-12-01 19:04:15 +0530

    Job_postings_operations-completed

--- Flask-05-Job-Match/app.py
+++ Flask-05-Job-Match/app.py
@@ -4,6 +4,22 @@ from config import MONGO_URI
 from pymongo import MongoClient
 from bson import ObjectId
 
+#Job-Seeker-modal
+{
+    "name": "Emmy Reek", 
+    "status": "Active", 
+    "skills": "Python, Flask Java", 
+    "experience": "Mid Level", 
+    "bio": "Passionate developer", 
+    "availability":"2023-12-01"
+}
+#Job_posting Model
+{
+    "title":"Java Developer",
+    "status":"Active",
+    "start_date":"2023-12-10",
+    "end_date":"2023-12-30"
+}
 
 
 
@@ -13,6 +29,7 @@ mongo = MongoClient(app.config['MONGO_URI'])
 
 #Creating database and modals
 job_seekers = mongo.job_machingdb.job_seekers
+job_postings = mongo.job_machingdb.job_postings
 
 #routes to initialze collections and add some data
 @app.route('/modal_init', methods=['GET'])
@@ -100,6 +117,59 @@ def delete_job_seeker_by_id(job_seeker_id) :
     
     except Exception as exp :
         return jsonify({'error':str(exp)}), 500
+#Job_seekers completed
+
+#Job_postings inits
+
+# API routes for Job Posting CRUD operations
+@app.route('/job_postings', methods=['GET'])
+def get_job_postings():
+    job_postings_list = list(job_postings.find())
+    return jsonify({'job_postings': str(job_postings_list)})
+
+@app.route('/job_postings/<id>', methods=['GET'])
+def get_job_posting(id):
+    try:
+        job_posting = job_postings.find_one({'_id': ObjectId(id)})
+        if job_posting:
+            return jsonify({'job_posting': str(job_posting)})
+        else:
+            return jsonify({'message': 'Job Posting not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/job_postings', methods=['POST'])
+def add_job_posting():
+    try:
+        data = request.json
+        job_posting_id = job_postings.insert_one(data).inserted_id
+        return jsonify({'job_posting_id': str(job_posting_id)})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/job_postings/<id>', methods=['PUT'])
+def update_job_posting(id):
+    try:
+        data = request.json
+        result = job_postings.update_one({'_id': ObjectId(id)}, {'$set': data})
+        if result.modified_count > 0:
+            return jsonify({'message': 'Job Posting updated successfully'})
+        else:
+            return jsonify({'message': 'Job Posting not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/job_postings/<id>', methods=['DELETE'])
+def delete_job_posting(id):
+    try:
+        result = job_postings.delete_one({'_id': ObjectId(id)})
+        if result.deleted_count > 0:
+            return jsonify({'message': 'Job Posting deleted successfully'})
+        else:
+            return jsonify({'message': 'Job Posting not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 
 
